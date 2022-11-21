91942b4a15dd84c0989cf8b8d0ddc8f3175a4991
mevshnu <vishnusivan1000@gmail.com>
2022-11-21 15:18:23 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println("press 1 to add data");
+        System.out.println("press 2 to view data");
+        System.out.println("press 3 to search data");
+        System.out.println("press 4 to delete data");
+        System.out.println("press 5 to exit");
+        Scanner sc = new Scanner(System.in);
+        int a =sc.nextInt():
+    }
+}
