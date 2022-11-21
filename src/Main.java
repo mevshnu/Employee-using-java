a1515e3d9576e820f6ca21d73e14fcc72c34008f
mevshnu <vishnusivan1000@gmail.com>
2022-11-21 16:17:47 +0530

    com

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 import java.util.ArrayList;
 public class Main {
     static ArrayList<ArrayList> user = new ArrayList<ArrayList>();
+    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args)
     {
         System.out.println("press 1 to add data");
@@ -9,7 +10,6 @@ public class Main {
         System.out.println("press 3 to search data");
         System.out.println("press 4 to delete data");
         System.out.println("press 5 to exit");
-        Scanner sc = new Scanner(System.in);
         int a =sc.nextInt();
     }
     static void addemployee()
@@ -40,6 +40,19 @@ public class Main {
         user.add(a1);
         System.out.println("employee added");
     }
-
+static void view()
+{
+    for(int i=0;i<user.size();i++)
+    {
+        System.out.println("employee code " + user.get(i).get(0));
+        System.out.println("employee name " + user.get(i).get(1));
+        System.out.println("employee designation " + user.get(i).get(2));
+        System.out.println("employee salary " + user.get(i).get(3));
+        System.out.println("employee company name " + user.get(i).get(4));
+        System.out.println("employee name " + user.get(i).get(5));
+        System.out.println("employee phone number " + user.get(i).get(6));
+        System.out.println("employee email" + user.get(i).get(7));
+    }
+}
 }
 
